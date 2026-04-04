f3fb120098b930829bb5f080656e86cb10ae8adb
CodeWithMihran <sohail.mihran@gmail.com>
2026-04-05 00:15:35 +0530

    Learned Folder Setup and Concept of Components

---
+++ React01 (Components)/src/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import Card from './components/Cards'
+import Navbar from './components/Navbar'
+
+const App = () => {
+  return (
+    <div>
+      <Navbar />
+      <Card />
+    </div>
+  )
+}
+
+export default App
