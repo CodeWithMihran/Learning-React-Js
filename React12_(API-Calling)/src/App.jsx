70010478149ef8b9d24d5d5a9414501387ac3ea3
CodeWithMihran <sohail.mihran@gmail.com>
2026-04-07 23:34:04 +0530

    Learned API-Calling in React

---
+++ React12_(API-Calling)/src/App.jsx
@@ -0,0 +1,58 @@
+// import React from 'react'
+// import axios from 'axios'
+
+// const App = () => {
+
+//   // const  getData = async ()=>{
+//   //   const response = await fetch('https://jsonplaceholder.typicode.com/users');
+//   //   const data = await response.json();
+
+//   //   console.log(response);
+//   //   console.log(data);
+//   // }
+
+//   const getData = async ()=>{
+//     // const response = await axios.get('https://jsonplaceholder.typicode.com/users');
+//     // console.log(response.data);
+
+//     const {data} = await axios.get('https://jsonplaceholder.typicode.com/users');
+//     console.log(data);
+//   }
+
+//   return (
+//     <div>
+//       <button onClick={getData}>Get Data</button>
+//     </div>
+//   )
+// }
+
+// export default App
+
+
+import React from 'react'
+import axios from 'axios'
+import { useState } from 'react'
+
+const App = () => {
+
+  const [data, setData] = useState([]);
+
+  const getData = async ()=>{
+    const response = await axios.get('https://picsum.photos/v2/list');
+    setData(response.data);
+
+  }
+
+  return (
+    <div>
+      <button onClick={getData}>Get Data</button>
+      <div>
+        {data.map(function(elem,idx){
+          return <h3>{idx+1}, Hello {elem.author}</h3>
+        })}
+      </div>
+    </div>
+  )
+}
+
+export default App
